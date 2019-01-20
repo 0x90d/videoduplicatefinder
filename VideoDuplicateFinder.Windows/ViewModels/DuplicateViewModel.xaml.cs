aeb5b4d04cab85e71612705d2a798d8b04c7337d
0x90d <46010672+0x90d@users.noreply.github.com>
2019-01-20 21:39:14 +0100

    Select file in explorer and catch external exceptions

--- VideoDuplicateFinder.Windows/ViewModels/DuplicateViewModel.xaml.cs
+++ VideoDuplicateFinder.Windows/ViewModels/DuplicateViewModel.xaml.cs
@@ -1,24 +1,23 @@
+using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using VideoDuplicateFinderWindows.Data;
 
-namespace VideoDuplicateFinderWindows.ViewModels
-{
-    public partial class DuplicateViewModel : UserControl
-    {
-        public DuplicateViewModel()
-        {
-            InitializeComponent();
-        }
-        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-	        if (e.ClickCount == 2) {
-		        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo {
-			        FileName = ((DuplicateItemViewModel)DataContext).Path,
-			        UseShellExecute = true
-		        });
-	        }
-        }
+namespace VideoDuplicateFinderWindows.ViewModels {
+	public partial class DuplicateViewModel : UserControl {
+		public DuplicateViewModel() {
+			InitializeComponent();
+		}
+		private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+			if (e.ClickCount != 2) return;
+			try {
+				System.Diagnostics.Process.Start("explorer.exe", $"/select, \"{((DuplicateItemViewModel)DataContext).Path}\"");
+			}
+			catch (Exception exception) {
+				MessageBox.Show(exception.Message, VideoDuplicateFinder.Windows.Properties.Resources.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
 
 	}
 }
