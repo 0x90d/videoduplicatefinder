95c1c84772603a8a06996979c9ce601fcf6aa119
0x90d <46010672+0x90d@users.noreply.github.com>
2021-08-21 10:31:12 +0200

    Undo #34afcc1

--- VDF.Core/Utils/CoreUtils.cs
+++ VDF.Core/Utils/CoreUtils.cs
@@ -22,7 +22,7 @@ namespace VDF.Core.Utils {
 		public static string CurrentFolder;
 		static CoreUtils() {
 			IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-			CurrentFolder = Path.GetDirectoryName(Environment.ProcessPath)!;
+			CurrentFolder = Path.GetDirectoryName(typeof(CoreUtils).Assembly.Location)!;
 		}
 	}
 }
