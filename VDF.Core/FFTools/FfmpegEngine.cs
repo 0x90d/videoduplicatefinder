ee8d631bd8c37a76af22c7acd7b437f828339c70
0x90d <46010672+0x90d@users.noreply.github.com>
2026-05-12 11:25:45 +0200

    Bump license-header year to 2026 across all source files

--- VDF.Core/FFTools/FfmpegEngine.cs
+++ VDF.Core/FFTools/FfmpegEngine.cs
@@ -1,5 +1,5 @@
-﻿// /*
-//     Copyright (C) 2025 0x90d
+// /*
+//     Copyright (C) 2026 0x90d
 //     This file is part of VideoDuplicateFinder
 //     VideoDuplicateFinder is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU Affero General Public License as published by
